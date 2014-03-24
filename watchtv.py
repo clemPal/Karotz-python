a020a0c7cfbb7e0df2787d4ebbc9bc0842f5737b
clemPal <clement.palliere@hotmail.fr>
2014-03-24 19:03:28 +0800

    modification of the ears position for when watchtv

--- watchtv.py
+++ watchtv.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 
-r_ears = requests.get('http://192.168.234.244/cgi-bin/ears?left=6&right=5') #place the ears in horizontal position. Note that the ears have a bug left=right+1.
+r_ears = requests.get('http://192.168.234.244/cgi-bin/ears?left=11&right=10') #place the ears in horizontal position. Note that the ears have a bug left=right+1.
 print r_ears.text
 
 r_led = requests.get('http://192.168.234.244/cgi-bin/leds?pulse=1&color=00ddff&speed=1550&color2=000000') #the LED is cian and blink slowly.
