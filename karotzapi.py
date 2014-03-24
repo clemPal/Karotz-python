c7e6e5d8564f7612eab9829702fae5ce7b328873
clemPal <clement.palliere@hotmail.fr>
2014-03-24 21:17:45 +0800

    watchtv.py call medicine.py

--- karotzapi.py
+++ karotzapi.py
@@ -9,15 +9,6 @@ def sleep():
 	#print r_led.text
 
 
-def watchtv():
-	r_ears = requests.get('http://192.168.234.244/cgi-bin/ears?left=11&right=10') #place the ears in horizontal position. Note that the ears have a bug left=right+1.
-	#print r_ears.text
-	r_led = requests.get('http://192.168.234.244/cgi-bin/leds?pulse=1&color=00ddff&speed=1550&color2=000000') #the LED is cian and blink slowly.
-	#print r_led.text
-	time.sleep(10)
-	execfile("slee.py") #execute slee.py
-
-
 def medicine():
 	r_ears = requests.get('http://192.168.234.244/cgi-bin/ears?left=16&right=15') #place the ears in vertical position. Note that the ears have a bug left=right+1.
 	#print r_ears.text
@@ -27,15 +18,28 @@ def medicine():
 	#print r_voice.text
 
 
+def watchtv():
+	r_ears = requests.get('http://192.168.234.244/cgi-bin/ears?left=11&right=10') #place the ears in horizontal position. Note that the ears have a bug left=right+1.
+	#print r_ears.text
+	r_led = requests.get('http://192.168.234.244/cgi-bin/leds?pulse=1&color=00ddff&speed=1550&color2=000000') #the LED is cian and blink slowly.
+	#print r_led.text
+	time.sleep(10)
+	sleep() #execute slee.py
+
+	time.sleep(20)
+	medicine()
+
+
+
 def relax ():
 	r_led = requests.get('http://192.168.234.244/cgi-bin/leds?pulse=1&color=00ff4d&speed=1550&color2=000000') #the LED is grenn and blink slowly.
 	#print r_led.text 
 	time.sleep(10)
-	execfile("slee.py") #execute slee.py
+	sleep() #execute slee.py
 
 
 '''
-Could be implemented in order to inform a new user who would script the function
+TO DO
 if __name__ == '__main__':
 
 	sleep()
